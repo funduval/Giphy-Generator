2dd0730188bd87bbf6d12c4b1fdac07e3dc9f1eb
funduval <fundaduval@gmail.com>
2017-05-20 20:35:48 -0400

    ok got some images on click

--- assets/javascript.js
+++ assets/javascript.js
@@ -30,50 +30,48 @@ $(".callName").on("click", function() {
             console.log(politician);
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + politician + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-            // Performing an AJAX GET request 
-
             $.ajax({
                 url: queryURL,
                 method: "GET"
             }).done(function(response) {
 
+              // Storing an array of results in the results variable
+              var results = response.data;
 
-                console.log(response);
+                console.log(results);
 
+                for (var i = 0; i < results.length; i++) {
 
-
-
-                //             // Looping over every result item
-                for (var i = 0; i < response.length; i++) {
-
-                    // Only taking action if the photo has an appropriate rating
-                    if (response[i].rating !== "r" && response[i].rating !== "pg-13") {
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
                         // Creating a div with the class "item"
                         var gifDiv = $("<div class='item'>");
 
                         // Storing the result item's rating
-                        var rating = response[i].rating;
+                        var rating = results[i].rating;
 
                         // Creating a paragraph tag with the result item's rating
                         var p = $("<p>").text("Rating: " + rating);
 
                         // Creating a place for the new gif
-                        var politicoGif = $("<div>");
+                        var politicoGif = $("<img>");
 
                         // Giving the image tag an src attribute of a proprty pulled off the
                         // result item
-                        politicoGif.attr("src", response[i].images.fixed_height.url);
+                        politicoGif.attr("src", results[i].images.fixed_height.url);
+                        console.log(results[i].images.fixed_height.url);
 
-                        // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                        // Appending the paragraph and personImageto the "gifDiv" div 
                         gifDiv.append(p);
                         gifDiv.append(politicoGif);
 
 
-                        $("#magicGifs").prepend(politicoGif);
+                        $("#magicGifs").prepend(gifDiv);
 
-                    }
+                    };
 
-                  }
+                  };
 
+
+            });
 
             });
